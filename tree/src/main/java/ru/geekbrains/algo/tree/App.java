ee5ab54b0c0a06287a95ef57a9fed9a8d6c2b32a
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-06-26 15:06:01 +0300

    Add stub for tree

---
+++ tree/src/main/java/ru/geekbrains/algo/tree/App.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.algo.tree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+    public static void main(String[] args) {
+
+        new App();
+    }
+    
+    
+
+    App() {
+
+    }
+
+}
