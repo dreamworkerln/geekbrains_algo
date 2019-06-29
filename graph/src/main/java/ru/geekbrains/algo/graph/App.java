936124490d5641ab941535fbcf00a90c6ef05c15
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-06-29 16:10:35 +0300

    init graph

---
+++ graph/src/main/java/ru/geekbrains/algo/graph/App.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.algo.graph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+    public static void main(String[] args) {
+
+        new App();
+    }
+    
+    
+
+    App() {
+
+    }
+
+}
