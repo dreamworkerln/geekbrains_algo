9870d2ef5352ed9874f47b357b45ca8ace344828
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-07-01 11:16:42 +0300

    draft

--- graph/src/main/java/ru/geekbrains/algo/graph/App.java
+++ graph/src/main/java/ru/geekbrains/algo/graph/App.java
@@ -1,5 +1,8 @@
 package ru.geekbrains.algo.graph;
 
+import ru.geekbrains.algo.graph.entities.DijkstrasAlgorithm;
+import ru.geekbrains.algo.graph.entities.Graph;
+
 /**
  * Hello world!
  *
@@ -15,6 +18,35 @@ public class App
     
 
     App() {
+
+
+        Graph graph = new Graph(10);
+        graph.addVertex("Москва");
+        graph.addVertex("Тула");
+        graph.addVertex("Рязань");
+        graph.addVertex("Калуга");
+        graph.addVertex("Тамбов");
+        graph.addVertex("Липецк");
+        graph.addVertex("Орел");
+        graph.addVertex("Саратов");
+        graph.addVertex("Курск");
+        graph.addVertex("Воронеж");
+
+        graph.addEdge("Москва","Тула", 182);
+        graph.addEdge("Москва","Калуга", 188);
+        graph.addEdge("Москва", "Рязань", 202);
+        graph.addEdge("Тула","Липецк", 294);
+        graph.addEdge("Рязань","Тамбов", 289);
+        graph.addEdge("Калуга","Орел", 214);
+        graph.addEdge("Липецк","Воронеж", 132);
+        graph.addEdge("Тамбов","Саратов", 378);
+        graph.addEdge("Орел","Курск", 170);
+        graph.addEdge("Саратов","Воронеж", 505);
+        graph.addEdge("Курск","Воронеж", 228);
+
+        graph.display();
+
+        DijkstrasAlgorithm.dijkstra(graph.getAdjMat(), 0, 8);
 
     }
 
---
+++ graph/src/main/java/ru/geekbrains/algo/graph/entities/Vertex.java
@@ -0,0 +1,63 @@
+package ru.geekbrains.algo.graph.entities;
+
+import java.util.Objects;
+
+public class Vertex {
+
+    private final String label;
+    private boolean visited;
+
+    //private Vertex previousVertex;
+
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(label, vertex.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+
+/*    @Override
+    public String toString() {
+        return "Vertex{" +
+                "label='" + label + '\'' +
+                '}';
+    }*/
+
+    @Override
+    public String toString() {
+        return label;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    /*
+    public Vertex getPreviousVertex() {
+        return previousVertex;
+    }
+
+    public void setPreviousVertex(Vertex previousVertex) {
+        this.previousVertex = previousVertex;
+    }
+    */
+}
